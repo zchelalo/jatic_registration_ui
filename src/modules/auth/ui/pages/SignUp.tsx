cfeb8e9afddc76fd50578bc3937b54813db92b68
zchelalo <eduardosaavedra687@gmail.com>
2024-10-08 13:24:38 -0700

    add: all logos of uts

--- src/modules/auth/ui/pages/SignUp.tsx
+++ src/modules/auth/ui/pages/SignUp.tsx
@@ -3,6 +3,15 @@ import { SignInStudentForm } from '@/modules/auth/ui/SignInStudentForm'
 import { SignInTeacherForm } from '@/modules/auth/ui/SignInTeacherForm'
 import { SignUpStudentForm } from '@/modules/auth/ui/SignUpStudentForm'
 
+import jatic from '@/assets/images/jatic.png'
+import utn from '@/assets/images/utn.png'
+import utnTi from '@/assets/images/utn_ti.png'
+import utEtchojoa from '@/assets/images/utetchojoa.png'
+import utHermosillo from '@/assets/images/uthermosillo.png'
+import utPuertoPeñasco from '@/assets/images/utpuerto_peñasco.png'
+import uts from '@/assets/images/uts.png'
+import utslrc from '@/assets/images/utslrc.png'
+
 enum Tab {
   REGISTRARSE = 'registrarse',
   INICIAR_SESION = 'iniciarSesion',
@@ -11,9 +20,9 @@ enum Tab {
 
 function SignUp() {
   return (
-    <div className='h-full w-full flex justify-center items-center p-4 sm:p-6'>
-      <div className='back-secondary w-full md:w-6/12 lg:w-5/12 xl:w-4/12 flex h-full rounded-lg p-2 sm:p-4 overflow-y-auto'>
-        <Tabs defaultValue={Tab.REGISTRARSE} className='h-full w-full'>
+    <div className='h-auto sm:h-full w-full flex flex-col sm:flex-row justify-center items-center p-4 sm:p-6'>
+      <div className='back-secondary w-full md:w-6/12 lg:w-5/12 xl:w-4/12 flex h-auto sm:h-full rounded-lg p-2 sm:p-4 sm:overflow-y-auto'>
+        <Tabs defaultValue={Tab.REGISTRARSE} className='h-auto sm:h-full w-full'>
 
           <TabsList className='flex justify-between w-full bg-transparent items-start'>
             <TabsTrigger value={Tab.REGISTRARSE} className='w-1/3 text-sm md:text-sm lg:text-lg rounded text-[var(--color-primary)] hover:text-[var(--color-secondary)] bg-transparent hover:bg-[var(--color-primary)]'>
@@ -41,13 +50,66 @@ function SignUp() {
 
         </Tabs>
       </div>
-      <div className='back md:w-6/12 lg:w-7/12 xl:w-8/12 hidden md:flex items-center justify-center h-full p-4'>
-        <img
-          src='https://jatic.mx/img/logo_jatic_2024_grande.png'
-          alt='logo_jatic_2024_grande'
-          className='max-h-full filter drop-shadow-xl'
-        />
-      </div>
+      <section className='back w-full md:w-6/12 lg:w-7/12 xl:w-8/12 flex flex-wrap items-center justify-center h-full p-4'>
+        <div className='w-11/12 sm:w-10/12 md:w-10/12 lg:w-8/12 xl:w-9/12 flex flex-wrap items-center justify-center'>
+          <figure className='w-full flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={jatic}
+              alt='Logo de JATIC'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+          <figure className='w-6/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={utn}
+              alt='Logo de UT Nogales'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+          <figure className='w-6/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={utnTi}
+              alt='Logo de TI UTN'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+          <figure className='w-6/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={utEtchojoa}
+              alt='Logo de UT Etchojoa'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+          <figure className='w-6/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={utHermosillo}
+              alt='Logo de UT Hermosillo'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+          <figure className='w-6/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={utPuertoPeñasco}
+              alt='Logo de UT Puerto Peñasco'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+          <figure className='w-6/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={uts}
+              alt='Logo de UT Sur de Sonora'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+          <figure className='w-6/12 sm:w-6/12 md:w-4/12 lg:w-3/12 xl:w-2/12 flex items-center justify-center p-8 sm:p-4'>
+            <img
+              src={utslrc}
+              alt='Logo de UT San Luis Río Colorado'
+              className='max-h-full filter drop-shadow-xl'
+            />
+          </figure>
+        </div>
+      </section>
     </div>
   )
 }
